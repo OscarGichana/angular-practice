1a4995343c07abdbf0e257c25f603c99adaa52de
Oscar Gichana <gichanaoscar3@gmail.com>
2020-12-18 13:18:04 +0300

    router parameters.

--- find/src/app/dishdetail/dishdetail.component.ts
+++ find/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 
 @Component({
@@ -16,9 +20,18 @@ export class DishdetailComponent implements OnInit {
   selectedDish = Dish
 
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private location: Location,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
+  
   }
 
 }
